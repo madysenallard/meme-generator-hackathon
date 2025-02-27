bba5c608175dfcd5855ba0edbfc4caa534285194
Madysen Allard <madysenallard7@gmail.com>
2025-02-27 09:56:40 -0700

    git flow mistake commit

---
+++ meme-generator/src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import "../Header/Header.scss";
+
+function Header() {
+  return (
+    <div className="header">
+      <h1 className="header__title">Meme Me This</h1>
+      <h3 className="header__subtitle">Automatic Meme Generator</h3>
+    </div>
+  );
+}
+
+export default Header;
